927233f18a53eee97757681cbb445ce5de5c6955
svan <svan@resnet.com>
2018-06-21 10:28:11 +0300

    renaming

---
+++ dailysearch.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DailySearch.aspx.cs" Inherits="DailySearch" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+	 <script type="text/javascript">
+	 	function expandall() {
+	 		var inputs, index;
+	 		inputs = document.getElementsByTagName('button');
+	 		for (index = 0; index < inputs.length; ++index) {
+	 			if (inputs[index].id.substring(0, 4) == "btn_")
+	 				inputs[index].style.display='none';
+	 		}
+	 		inputs = document.getElementsByTagName('div');
+	 		for (index = 0; index < inputs.length; ++index) {
+	 			if (inputs[index].id.substring(0, 6) == "child_")
+	 				inputs[index].style.display = 'inline';
+	 		} 	
+	 	}
+	 </script>
+</head>
+<body>
+    <form id="form1" defaultfocus="SearchText" runat="server">
+    <div>
+    
+    	<br />
+		 <asp:Panel ID="Panel2" runat="server" HorizontalAlign="Center">
+			 <table style="width:100%;">
+				 <tr>
+					 <td>
+						 <br />
+					 </td>
+					 <td>
+						 <asp:Image ID="Image1" runat="server" ImageUrl="~/IMAGES/search.PNG" />
+					 </td>
+					 <td>
+						 &nbsp;</td>
+				 </tr>
+			 </table>
+			 <br />
+			 	<asp:Label ID="Label2" runat="server" Font-Names="Arial" Font-Size="10pt" 
+				 Text="Type:"></asp:Label>
+			 	<asp:DropDownList ID="OptionsList" runat="server" Width="150pt">
+					<asp:ListItem>Day</asp:ListItem>
+					<asp:ListItem>Tomorrow</asp:ListItem>
+					<asp:ListItem>All</asp:ListItem>
+				 </asp:DropDownList>
+				 <asp:Label ID="Label3" runat="server" Font-Names="Arial" Font-Size="10pt" 
+				 Text="User:"></asp:Label>
+				 <asp:DropDownList ID="PersonsList" runat="server" Width="150pt">
+				 </asp:DropDownList>
+			 <asp:Label ID="Label4" runat="server" Font-Names="Arial" Font-Size="10pt" 
+				 Text="Text:"></asp:Label>
+			 <asp:TextBox ID="SearchText" runat="server" Width="340px" 
+				 ontextchanged="SearchText_TextChanged"></asp:TextBox>
+			 <asp:Button ID="SearchButton" runat="server" 
+	Text="Search" onclick="SearchButton_Click" />
+		 	 <br />
+		 </asp:Panel>
+								<input id="ExpandButton" onclick = "expandall();" type="button" value="Expand all..." /><br />
+    
+    </div>
+		 <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+			 ConnectionString="<%$ ConnectionStrings:ConnectionString %>" 
+			 ProviderName="System.Data.OleDb" 
+			 
+		 
+		 SelectCommand="SELECT REPORTS.TIME_START, REPORTS.TIME_END, REPORTS.REPORT_DATE, REPORTS.REPORT_DONE, REPORTS.REPORT_FUTURE, PERSONS.PERSON_NAME FROM PERSONS , REPORTS WHERE PERSONS.PERSON_ID = REPORTS.PERSON_ID AND ( REPORTS.REPORT_DONE LIKE '%memo%' OR REPORTS.REPORT_FUTURE LIKE '%memo%')"></asp:SqlDataSource>
+		 <center>
+		 <asp:GridView ID="SearchGridView" runat="server" AutoGenerateColumns="False" 
+			 DataSourceID="SqlDataSource1" AllowPaging="True" AllowSorting="True" 
+			 onrowdatabound="SearchGridView_RowDataBound" PageSize="30">
+			 <PagerSettings Mode="NumericFirstLast" Position="TopAndBottom" />
+			 <RowStyle Font-Names="Arial" Font-Size="10pt" />
+			 <Columns>
+				 <asp:TemplateField HeaderText="#"></asp:TemplateField>
+				 <asp:BoundField DataField="PERSON_NAME" HeaderText="PERSON_NAME" 
+					 SortExpression="PERSON_NAME" />
+				 <asp:TemplateField HeaderText="REPORT_DATE" SortExpression="REPORT_DATE">
+					 <EditItemTemplate>
+						 <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("REPORT_DATE") %>'></asp:TextBox>
+					 </EditItemTemplate>
+					 <ItemTemplate>
+						 <asp:Label ID="Label1" runat="server" Text='<%# Bind("REPORT_DATE") %>'></asp:Label>
+					 </ItemTemplate>
+				 </asp:TemplateField>
+				 <asp:BoundField DataField="REPORT_DONE" HeaderText="REPORT_DONE" 
+					 SortExpression="REPORT_DONE" />
+				 <asp:BoundField DataField="REPORT_FUTURE" HeaderText="REPORT_FUTURE" 
+					 SortExpression="REPORT_FUTURE" />
+			 </Columns>
+		 	 <PagerStyle BackColor="#3399FF" Font-Names="Arial" Font-Size="10pt" 
+				 HorizontalAlign="Center" VerticalAlign="Middle" />
+		 	<HeaderStyle Font-Names="Arial" Font-Size="10pt" />
+		 </asp:GridView>
+		 </center>
+    </form>
+</body>
+</html>
---
+++ machines.aspx.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI.WebControls;
+using GTOHELPER;
+using System.Data;
+using System.Diagnostics;
+
+public partial class Machines : GTOHelper
+{
+	string Machine
+	{
+		get
+		{
+			object res = Request.QueryString["machine"];
+			if (res == null)
+				return "";
+			return res.ToString();
+		}
+	}
+	private void WakeFunction(string MAC_ADDRESS)
+	{
+		Process process = new Process();
+		process.StartInfo.RedirectStandardOutput = true;
+		process.StartInfo.UseShellExecute = false;
+		process.StartInfo.CreateNoWindow = true;
+		process.StartInfo.FileName = HttpRuntime.AppDomainAppPath + "bin\\WolCmd.exe";
+		process.StartInfo.Arguments = MAC_ADDRESS + " 192.168.0.1 255.255.255.0 3";
+		process.Start();
+	}
+	private void ShutDownFunction(string pc)
+	{
+		Process process = new Process();
+		process.StartInfo.RedirectStandardOutput = true;
+		process.StartInfo.UseShellExecute = false;
+		process.StartInfo.CreateNoWindow = true;
+		process.StartInfo.FileName = "shutdown";
+		process.StartInfo.Arguments = string.Format(@"/s /m \\{0} /t 0", pc);
+		process.Start();
+	}
+	protected System.Web.UI.Control FindControlbyID(string ID, System.Web.UI.Control parent = null)
+	{
+		if (parent == null)
+		{
+			parent = this;
+			return FindControlbyID(ID, parent);
+		}
+
+		foreach (System.Web.UI.Control ctrl in parent.Controls)
+		{
+			if (ctrl.ID == ID)
+				return ctrl;
+
+			System.Web.UI.Control ct = FindControlbyID(ID, ctrl);
+			if (ct != null)
+				return ct;
+		}
+		return null;
+	}
+	protected void AddCtrl(string txt)
+	{
+		System.Web.UI.Control ct = FindControlbyID("maindiv");
+		if (ct == null)
+			return;
+
+		Literal l = new Literal();
+		l.Text = txt;
+		ct.Controls.Add(l);
+	}
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if (string.IsNullOrEmpty(Machine))
+		{
+			hint.InnerHtml = "Select Machine:";
+
+			if (Request.QueryString["find"] != null)
+			{
+				List<string> ComputerNames = GetComputers();
+				foreach (string pc in ComputerNames)
+				{
+					AddCtrl(string.Format("<button type = 'button' class='btn btn-block btn-primary btn-lg machine'>{0}</button>", pc));
+				}
+			}
+			else
+			{
+				AddCtrl(string.Format("<button type = 'button' class='btn btn-block btn-success btn-lg find'>{0}</button>", "Find machines"));
+				foreach (DataRow rowCur in GetDataSet("Select PCNAME from PCS").Tables[0].Rows)
+				{
+					AddCtrl(string.Format("<button type = 'button' class='btn btn-block btn-primary btn-lg machine'>{0}</button>", rowCur["PCNAME"].ToString()));
+				}
+			}
+		}
+		else
+		{
+			hint.InnerHtml = string.Format("Select Action For {0}:", Machine);
+
+			AddCtrl("<button type = 'button' class='btn btn-block btn-primary btn-lg maction' id='wake'>Wake Up</button>");
+			AddCtrl("<button type = 'button' class='btn btn-block btn-primary btn-lg maction' id='shut'>Shut Down</button>");
+			AddCtrl("<button type = 'button' class='btn btn-block btn-primary btn-lg maction' id='scan'>Scan for MAC</button>");
+			AddCtrl("<button type = 'button' class='btn btn-block btn-primary btn-lg maction' id='remove'>Remove</button>");
+			AddCtrl("<button type = 'button' class='btn btn-block btn-primary btn-lg back'>Back</button>");
+
+			if (Request.QueryString["scan"] != null)
+			{
+				SQLExecute(string.Format("INSERT INTO PCS (PCNAME) SELECT '{0}' WHERE NOT EXISTS(SELECT PCNAME FROM PCS WHERE PCNAME = '{1}')", Machine, Machine));
+				string mac = GetMAC(Machine);
+				if (!string.IsNullOrEmpty(mac))
+				{
+					SQLExecute(string.Format("UPDATE PCS set MAC = '{0}' WHERE PCNAME = '{1}'", GetMAC(Machine), Machine));
+				}
+			}
+			else if (Request.QueryString["shut"] != null)
+			{
+				ShutDownFunction(Machine);
+			}
+			else if (Request.QueryString["remove"] != null)
+			{
+				SQLExecute(string.Format("DELETE FROM PCS WHERE PCNAME = '{0}'", Machine));
+				Response.Redirect(CurrentPageName);
+			}
+			else if (Request.QueryString["wake"] != null)
+			{
+				foreach (DataRow rowCur in GetDataSet(string.Format("SELECT MAC FROM PCS WHERE PCNAME = '{0}'", Machine)).Tables[0].Rows)
+				{
+					string mac = rowCur["MAC"].ToString();
+					foreach (string onemac in mac.Split(' '))
+					{
+						WakeFunction(onemac);
+					}
+				}
+			}
+		}
+	}
+	public static List<string> GetComputers()
+	{
+		List<string> ComputerNames = new List<string>();
+
+		DirectoryEntry entry = new DirectoryEntry("LDAP://mps");
+		DirectorySearcher mySearcher = new DirectorySearcher(entry);
+		mySearcher.Filter = ("(objectClass=computer)");
+		mySearcher.SizeLimit = int.MaxValue;
+		mySearcher.PageSize = int.MaxValue;
+
+		foreach (SearchResult resEnt in mySearcher.FindAll())
+		{
+			string ComputerName = resEnt.GetDirectoryEntry().Name;
+			if (ComputerName.StartsWith("CN="))
+				ComputerName = ComputerName.Remove(0, "CN=".Length);
+			ComputerNames.Add(ComputerName);
+		}
+
+		mySearcher.Dispose();
+		entry.Dispose();
+
+		return ComputerNames;
+	}
+}
