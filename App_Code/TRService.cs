3e770adcda1c9aea62075202d000374e7b1928a3
svan <svan@resnet.com>
2018-08-14 17:33:24 +0300

    removed old files

--- App_Code/TRService.cs
+++ App_Code/TRService.cs
@@ -4,7 +4,6 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
-using GTOHELPER;
 using System.Net.Mail;
 using System.Globalization;
 using System.DirectoryServices;
